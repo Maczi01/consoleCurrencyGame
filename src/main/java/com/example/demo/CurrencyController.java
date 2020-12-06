f1f8a614a83b6ead5ef2678afb7722aeeb24a623
Maczi01 <mateusz.w.twardy@gmail.com>
2020-12-06 19:18:51 +0100

    Refactor part 1

--- src/main/java/com/example/demo/CurrencyController.java
+++ src/main/java/com/example/demo/CurrencyController.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 import org.apache.commons.lang3.math.NumberUtils;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +10,7 @@ import java.util.Scanner;
 public class CurrencyController {
 
     private Scanner scanner = new Scanner(System.in);
-    private double value;
+    private final String LINK_TO_API = "https://v6.exchangerate-api.com/v6/ab33143b55d3dc0b1f57ddd3/latest/eur";
 
     public CurrencyController() {
         getCurrencyAndValueFromUser();
@@ -21,7 +19,7 @@ public class CurrencyController {
     public double getCurrency() {
         var restTemplate = new RestTemplate();
         Currency currency = restTemplate
-                .getForObject("https://v6.exchangerate-api.com/v6/ab33143b55d3dc0b1f57ddd3/latest/eur", Currency.class);
+                .getForObject(LINK_TO_API, Currency.class);
         Double pln = currency.getConversionRates().getPLN();
         System.out.println(pln);
         return pln;
@@ -29,15 +27,14 @@ public class CurrencyController {
 
     public double getValueFromUser() {
         String valueToCheck;
+            System.out.println("Sprobuj odgadnac dzisiejszy kurs EUR w przeliczeniu na PLN");
         do {
             System.out.println("Podaj wartosc");
             valueToCheck = scanner.nextLine();
         } while (!isConvertibleToDouble(valueToCheck));
-//        scanner.close();
         return Double.parseDouble(valueToCheck);
     }
 
-//    @EventListener(ApplicationReadyEvent.class)
     public void getCurrencyAndValueFromUser() {
         double currency = getCurrency();
         double valueFromUser = getValueFromUser();
@@ -45,34 +42,27 @@ public class CurrencyController {
             valueFromUser = getValueFromUser();
         }
         System.out.println("Wygrales!");
-
     }
 
     private boolean compareValues(double currency, double valueFromUser) {
         if (currency == valueFromUser) {
             return true;
         } else if (currency > valueFromUser) {
-            System.out.println("Za duzo!");
+            notEnoughMessage();
             return false;
         } else
-            System.out.println("za malo!");
+            tooMuchMessage();
         return false;
     }
 
 
     private boolean isConvertibleToDouble(String valueToCheck) {
         boolean parsable = NumberUtils.isParsable(valueToCheck);
-        if (parsable) {
-            System.out.println("ok, dobra wartosc");
-        } else {
-            System.out.println("jeszcze raz");
+        if (!parsable) {
+            System.out.println("Nieprawidłowy format, sprobuj ponownie");
         }
         return parsable;
     }
-
-//    public void showMenu() {
-//        System.out.println("Ile PLN musisz przeznaczyc na zakup 1 euro?");
-//    }
 
     public void tooMuchMessage() {
         System.out.println("Za duzo!");
